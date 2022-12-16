3cb056e345d387b987a704a99cefb5d1db090213
Mandimby RAVELOARINJAKA <mandimby.raveloarinjaka@gmail.com>
2022-12-16 15:18:21 -0500

    day 10: move larger sample to a dedicated file

--- src/day_10.clj
+++ src/day_10.clj
@@ -10,153 +10,7 @@
 addx 3
 addx -5")
 
-(def LARGER-SAMPLE
-"addx 15
-addx -11
-addx 6
-addx -3
-addx 5
-addx -1
-addx -8
-addx 13
-addx 4
-noop
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx -35
-addx 1
-addx 24
-addx -19
-addx 1
-addx 16
-addx -11
-noop
-noop
-addx 21
-addx -15
-noop
-noop
-addx -3
-addx 9
-addx 1
-addx -3
-addx 8
-addx 1
-addx 5
-noop
-noop
-noop
-noop
-noop
-addx -36
-noop
-addx 1
-addx 7
-noop
-noop
-noop
-addx 2
-addx 6
-noop
-noop
-noop
-noop
-noop
-addx 1
-noop
-noop
-addx 7
-addx 1
-noop
-addx -13
-addx 13
-addx 7
-noop
-addx 1
-addx -33
-noop
-noop
-noop
-addx 2
-noop
-noop
-noop
-addx 8
-noop
-addx -1
-addx 2
-addx 1
-noop
-addx 17
-addx -9
-addx 1
-addx 1
-addx -3
-addx 11
-noop
-noop
-addx 1
-noop
-addx 1
-noop
-noop
-addx -13
-addx -19
-addx 1
-addx 3
-addx 26
-addx -30
-addx 12
-addx -1
-addx 3
-addx 1
-noop
-noop
-noop
-addx -9
-addx 18
-addx 1
-addx 2
-noop
-noop
-addx 9
-noop
-noop
-noop
-addx -1
-addx 2
-addx -37
-addx 1
-addx 3
-noop
-addx 15
-addx -21
-addx 22
-addx -6
-addx 1
-noop
-addx 2
-addx 1
-noop
-addx -10
-noop
-noop
-addx 20
-addx 1
-addx 2
-addx 2
-addx -6
-addx -11
-noop
-noop
-noop")
+(def LARGER-SAMPLE (slurp (io/resource "day_10_mid_size.txt")))
 
 (defn parse-instruction
   [[instruction]]
@@ -228,19 +82,16 @@ noop")
         (reduce (fn [ctx _] (update-on-tick ctx)) new-context (get new-context :X-values))))))
 
 (process-instructions (->instructions SAMPLE))
-(process-instructions (->instructions LARGER-SAMPLE))
+#_(process-instructions (->instructions LARGER-SAMPLE))
 
+(defn compute-signal-strength
+  [input]
+  (->> (process-instructions (->instructions input))
+       :signal-strength
+       (drop 19)
+       (partition 40 40 [])
+       (map first)
+       (reduce +)))
 
-(->> (process-instructions (->instructions LARGER-SAMPLE))
-     :signal-strength
-     (drop 19)
-     (partition 40 40 [])
-     (map first)
-     (reduce +))
-
-(->> (process-instructions (->instructions (slurp (io/resource "day_10.txt"))))
-     :signal-strength
-     (drop 19)
-     (partition 40 40 [])
-     (map first)
-     (reduce +))
+(compute-signal-strength LARGER-SAMPLE)
+#_(compute-signal-strength (slurp (io/resource "day_10.txt")))
